9a6610b52e18097cb094e1375f393e1651526de7
python3 <python3@bumjinui-MacBookPro.local>
2019-11-20 10:30:31 +0900

    .

--- src/plugins/socketPlugin.js
+++ src/plugins/socketPlugin.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 
-const socket = io('http://169.56.92.40:8083');
+//const socket = io('http://169.56.92.40:8083');
 //const socket = io('https://bay4u.co.kr/chatif');
+const socket = io('https://bay4u.co.kr:8083');
 
 
 const SocketPlugin = {
