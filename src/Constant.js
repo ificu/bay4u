ac247894c0155638ce96753326b5a511d775b7bb
bumjinchoi <bj.choi.ificu@gmail.com>
2019-12-12 18:10:48 +0900

    .

--- src/Constant.js
+++ src/Constant.js
@@ -1,7 +1,7 @@
 export default {
     PUSH_MSG_DATA: 'pushMsgData',
 
-    /* 운영계 */
+    /* 운영계 
     LAMBDA_URL: 'https://2fb6f8ww5b.execute-api.ap-northeast-2.amazonaws.com/bay4u/backendService',
     SCPIF_URL: 'https://bay4u.co.kr/scpif/',
     GOOGLE_URL: 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyApWMx0PYvexvPKJkmMA9lAwWvMC5K6FZU',
@@ -10,9 +10,9 @@ export default {
     INTRA_PKGIF_URL: 'https://bay4u.co.kr/extpkgif',
     CHAT_URL: 'https://bay4u.co.kr:8083',
     JSON_HEADER: {'Accept': 'application/json','Content-Type': 'application/json'},
-    AIBRIL_HEADER: {'api-key': '7bfee9e0-b19a-4f14-a3a0-42c259aac9f2', 'Content-Type': 'application/json'}
+    AIBRIL_HEADER: {'api-key': '7bfee9e0-b19a-4f14-a3a0-42c259aac9f2', 'Content-Type': 'application/json'}*/
 
-    /* 개발계 
+    /* 개발계 */
     LAMBDA_URL: 'https://mhq81lt4bf.execute-api.ap-northeast-2.amazonaws.com/prod/backendServiceTest',
     SCPIF_URL: 'https://bay4u.co.kr:444/scpif/',
     GOOGLE_URL: 'https://vision.googleapis.com/v1/images:annotate?key=AIzaSyApWMx0PYvexvPKJkmMA9lAwWvMC5K6FZU',
@@ -21,5 +21,5 @@ export default {
     INTRA_PKGIF_URL: 'https://bay4u.co.kr:444/extpkgif',
     CHAT_URL: 'https://bay4u.co.kr:8084',    
     JSON_HEADER: {'Accept': 'application/json','Content-Type': 'application/json'},
-    AIBRIL_HEADER: {'api-key': '7bfee9e0-b19a-4f14-a3a0-42c259aac9f2', 'Content-Type': 'application/json'}*/
+    AIBRIL_HEADER: {'api-key': '7bfee9e0-b19a-4f14-a3a0-42c259aac9f2', 'Content-Type': 'application/json'}
   };
